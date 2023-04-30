7238e2e6ee99803a9ababcce0ca966f77aaf2977
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2023-05-01 03:10:50 +0900

    router

--- nextjs-blog/components/Layout.js
+++ nextjs-blog/components/Layout.js
@@ -22,7 +22,18 @@ export default function Layout({ children }) {
         </a>
       </footer>
 
-      <style jsx>{`
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
         main {
           padding: 5rem 0;
           flex: 1;
@@ -56,20 +67,6 @@ export default function Layout({ children }) {
           font-size: 1.1rem;
           font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
             DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
         }
       `}</style>
     </div>
