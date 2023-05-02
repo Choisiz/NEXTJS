af2939916f24c4294d09e9d5e5c90c6e6a6eb50c
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2023-05-03 01:34:19 +0900

    api

---
+++ nextjs-blog/pages/api/user.js
@@ -0,0 +1,4 @@
+import { userDetail } from "../constants/useDetail";
+export default function handler(req, res) {
+  res.status(200).json(userDetail);
+}
---
+++ nextjs-blog/pages/constants/useDetail.js
@@ -0,0 +1,6 @@
+export const userDetail = {
+  name: "choisiz boy",
+  age: 20,
+  height: 185,
+  weight: 80,
+};
