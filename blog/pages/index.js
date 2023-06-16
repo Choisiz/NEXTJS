82f8592554f5151ce20a49379c63d149df76a6b2
KyungYeul Choi <dnjsvltm201311681@gmail.com>
2023-06-16 17:38:54 +0900

    blog-project datafetching

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,8 +1,39 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+//import { getSortedPostsData } from "../lib/post";
+import { useEffect, useState } from "react";
 
-export default function Home() {
+//SSG
+//export async function getStaticProps() {
+//  const allPostsData = getSortedPostsData();
+//  return {
+//    props: {
+//      allPostsData,
+//    },
+//  };
+//}
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3000//api/posts");
+  const json = await response.json();
+  return {
+    props: {
+      allPostsData: json.allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  //csr
+  //const [allPostsData, setAllPostsData] = useState([]);
+  //useEffect(() => {
+  //  fetch("/api/posts", {
+  //    method: "GET",
+  //  })
+  //    .then((res) => res.json())
+  //   .then((data) => setAllPostsData(data.allPostsData));
+  //}, []);
   return (
     <Layout home>
       <Head>
@@ -11,9 +42,24 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
+          (This iss a sample website - you’ll be building a site like this on{" "}
           <a href="/posts/first-post">first post page</a>.)
         </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
