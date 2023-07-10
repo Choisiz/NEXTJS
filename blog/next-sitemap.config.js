51e65bee21724d1cf03ccabf8c12be5d9fb7b1b1
KyungYeul Choi <dnjsvltm201311681@gmail.com>
2023-07-10 16:27:23 +0900

    eslint

--- blog/next-sitemap.config.js
+++ blog/next-sitemap.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
+// eslint-disable-next-line no-undef
 module.exports = {
   siteUrl: "https://nextjs-blog-gamma-six-65.vercel.app",
   generateRobotsTxt: true, // (optional)
